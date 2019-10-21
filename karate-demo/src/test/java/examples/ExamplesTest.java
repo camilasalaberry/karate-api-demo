f9a4ec454345badc11b4bd25c5b923bec75c299d
camila-paula <camila-paula@procergs.rs.gov.br>
2019-10-21 15:17:21 -0300

    demo karate

---
+++ karate-demo/src/test/java/examples/ExamplesTest.java
@@ -0,0 +1,10 @@
+package examples;
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+class ExamplesTest {
+    // this will run all *.feature files that exist in sub-directories
+    // refer to https://github.com/intuit/karate#naming-conventions
+    // for folder-structure recommendations and naming conventions
+}
